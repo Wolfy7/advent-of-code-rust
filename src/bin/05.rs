b5328a6a2e4d30aec5de2ba56d821c05e299f7ed
Wolfy7 <andre.decher@gmail.com>
2024-12-05 23:05:48 +0100

    solution day 5 part 2

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -64,8 +64,73 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(sum)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    Some(123)
+pub fn part_two(input: &str) -> Option<u32> {
+    let mut page_ordering_rules = Vec::new();
+    let mut page_numbers = Vec::new();
+    let mut section1 = true;
+
+    for line in input.lines() {
+        if section1 {
+            let mut parts = line.split("|");
+            if let (Some(a), Some(b)) = (parts.next(), parts.next()) {
+                page_ordering_rules.push((a.parse::<i32>().unwrap(), b.parse::<i32>().unwrap()));
+            }
+
+            if line.is_empty() {
+                section1 = false;
+            }
+        } else {
+            let parts: Vec<i32> = line
+                .split(",")
+                .filter_map(|s| s.parse::<i32>().ok())
+                .collect();
+
+            let mut page_hash_map = HashMap::new();
+            for (i, n) in parts.iter().enumerate() {
+                page_hash_map.insert(*n, i);
+            }
+
+            page_numbers.push(page_hash_map);
+        }
+    }
+
+    let mut pages: Vec<Vec<i32>> = Vec::new();
+    for update in page_numbers {
+        for (from, to) in page_ordering_rules.iter() {
+            if !update.contains_key(from) || !update.contains_key(to) {
+                continue;
+            }
+
+            let from_index = update.get(from).unwrap();
+            let to_index = update.get(to).unwrap();
+
+            if from_index > to_index {
+                pages.push(update.keys().cloned().collect());
+                break;
+            }
+        }
+    }
+
+    let mut sum: u32 = 0;
+    for page in pages.iter_mut() {
+        page.sort_by(|a, b| {
+            for &(x, y) in page_ordering_rules.iter() {
+                if x == *a && y == *b {
+                    return std::cmp::Ordering::Less; // a vor b
+                } else if x == *b && y == *a {
+                    return std::cmp::Ordering::Greater; // b vor a
+                }
+            }
+            std::cmp::Ordering::Equal
+        });
+
+        let middle = page.len() / 2;
+        sum += page[middle] as u32;
+    }
+
+    // println!("{:?}", pages);
+
+    Some(sum)
 }
 
 #[cfg(test)]
